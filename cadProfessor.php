d5d37b4c81a028d5f47d285176842761477e61bf
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-12-13 00:41:02 -0400

    preparando os icons

--- cadProfessor.php
+++ cadProfessor.php
@@ -3,11 +3,7 @@
 require_once 'vendor/autoload.php';
 
 $prof = new \App\Model\Professor;
-//$prof->setId(8);
-//$prof->setNome("Black da Silva Jr");
-//$prof->setDtNascimento('2021-12-11');
 $profDAO = new App\ModelDao\ProfessorDao;
-//$profDAO->updateProfessor($prof);
 
 include __DIR__.'/includes/Header/index.php';
 include __DIR__.'/includes/Form/formProfessor.php';
